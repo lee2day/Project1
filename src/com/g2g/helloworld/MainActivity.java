05d2a2a3f2e10910df39e0025bf2abaf989d6ed4
Great_Ha <Great_Ha@Great_Ha-PC>
2012-10-14 23:42:35 +0900

    2012.10.14 메일 Activity layout 구성단계

--- src/com/g2g/helloworld/MainActivity.java
+++ src/com/g2g/helloworld/MainActivity.java
@@ -4,12 +4,35 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+
+import android.content.Intent;
+
 public class MainActivity extends Activity {
 
+	// 이벤트 리스너 인터페이스 멥버 변수 선언 및 정의
+	private OnClickListener btn3_click_listener = new OnClickListener() {
+		public void onClick(View v) {
+			// 다음 activity 실행 요청
+			startActivity(new Intent(MainActivity.this, MailActivity.class));
+			// 애니메이션 효과 (fade in)
+			overridePendingTransition(R.anim.fade, R.anim.hold);
+		}
+	};
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // 리소스에 정의되어 있는 버튼 ID
+        Button button03 = (Button)findViewById(R.id.button03);
+        // 버튼에 멤버변수 리스너 설정
+        button03.setOnClickListener(btn3_click_listener);
     }
 
     @Override
